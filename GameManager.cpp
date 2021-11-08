5492e1fe3d793283ceec40cbc5c4b5893e620943
GriffinSpeidel <griffin.speidel@gmail.com>
2021-11-08 18:44:49 -0500

    made gem game

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,6 +1,7 @@
 #include "GameManager.h"
 
 #include "ExampleGame.h"
+#include "GemGame.h"
 
 #include <iostream>
 #include <string>
@@ -40,4 +41,6 @@ void GameManager::AddGames(){
     //insert your game below like the example line
     ExampleGame* eg = new ExampleGame();
     m_games.insert(make_pair("example", eg));
+    GemGame* gg = new GemGame();
+    m_games.insert(make_pair("gem", gg));
 }
