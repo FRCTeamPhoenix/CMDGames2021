c28abf00237387f1a68b69b934c1f80a1a002706
epicyguy <63820895+epicyguy@users.noreply.github.com>
2021-11-08 18:59:48 -0500

    Created RockPaperScissor

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,6 +1,7 @@
 #include "GameManager.h"
 
 #include "ExampleGame.h"
+#include "RockPaperScissor.h"
 
 #include <iostream>
 #include <string>
@@ -40,4 +41,6 @@ void GameManager::AddGames(){
     //insert your game below like the example line
     ExampleGame* eg = new ExampleGame();
     m_games.insert(make_pair("example", eg));
+    RockPaperScissor* rps = new RockPaperScissor();
+    m_games.insert(make_pair("rock paper scissor", rps));
 }
